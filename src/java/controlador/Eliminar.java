800ebcce328774c1ba284c15fe3c9fb44f88d503
julissa salinas <salinasjulissa6@gmail.com>
2020-06-13 17:20:31 -0600

    ya hice el controlador eliminar.java. Signed-off-by:julissa salinas <salinasjulissa6@gmail.com>

---
+++ src/java/controlador/Eliminar.java
@@ -0,0 +1,61 @@
+
+package controlador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import modelo.Persona;
+
+public class Eliminar extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Eliminar</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Eliminar at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         // processRequest(request, response);
+        
+        //Metodo para la Eliminacion de datos
+        String dui_persona = request.getParameter("dui");
+        Persona person = new Persona();
+        person.setDui(dui_persona);
+        
+        if (person.eliminar()){
+            request.getRequestDispatcher("Delete.jsp").forward(request, response);
+        }else{
+            request.getRequestDispatcher("noexito.jsp").forward(request, response);
+        }
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
